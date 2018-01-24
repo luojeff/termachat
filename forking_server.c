356a11b5c546a0cf171b4ea963ce6bc9eee1684f
Jeffrey <luojeffrey@yahoo.com>
2018-01-24 17:36:31 -0500

    cleaned comments

--- forking_server.c
+++ forking_server.c
@@ -20,7 +20,8 @@ int repeat = 1;
 int GPORT = PORT;
 
 int chatrooms_added = 0;
-struct chatroom existing_chatrooms[MAX_NUM_CHATROOMS];  
+struct chatroom existing_chatrooms[MAX_NUM_CHATROOMS];
+
 
 // KEPT FOR TESTING PURPOSES
 void _subprocess(int socket, char *group_name) {  
@@ -38,6 +39,7 @@ void _subprocess(int socket, char *group_name) {
 }
 
 
+/*
 static void sighandler(int signo){
   switch(signo) {
   case SIGINT:
@@ -48,6 +50,7 @@ static void sighandler(int signo){
     break;
   }
 }
+*/
 
 
 void intHandler(int sig){
@@ -59,40 +62,37 @@ int main() {
   struct sigaction act;
   act.sa_handler = intHandler;
   sigaction(SIGINT, &act, NULL);
-  //signal(SIGINT, sighandler);
-  //signal(SIGUSR1, sighandler);
+  
   
   int global_listen_socket;
-  if((global_listen_socket = server_setup(GPORT++)) != -1){
+  if((global_listen_socket = server_setup(GPORT++)) != -1)
     printf("[MAIN %d]: Main server successfully created!\n", getpid());
-  } else {
+  else
     print_error();
-  }
+  
   
   // Unnamed pipe between mainserver and listeningserver
   int pipe_to_main[2];
   pipe2(pipe_to_main, O_NONBLOCK);
   
-  // To be implemented...
-  char *fifos[MAX_NUM_CHATROOMS];
-  
   int f = fork();
-  if(f == 0) {
+  if(f == 0)
     mainserver(pipe_to_main);
-  } else if (f > 0) {
+  else if (f > 0)
     listening_server(global_listen_socket, pipe_to_main);
-  } else {
+  else 
     print_error();
-  }
 }
 
 
-/* Listens for incoming connections from other clients */
+// Listens for incoming connections from other clients
 void listening_server(int global_listen_socket, int pipe_to_main[2]){
+  
   int global_client_socket;
-
   close(pipe_to_main[0]);
+  
   while(cont){
+    
     if((global_client_socket = server_connect(global_listen_socket)) != -1){
       printf("[MAIN %d]: Sockets connected!\n", getpid());
     } else {
@@ -125,11 +125,11 @@ void listening_server(int global_listen_socket, int pipe_to_main[2]){
 void mainserver(int pipe_to_main[2]){
   close(pipe_to_main[WRITE]);
   
-  int fd;
-  int count = 0;
-  int server_fds[MAX_CLIENTS];
-  int sem_ids[MAX_CLIENTS];
+  int fd, i;
+  int sub_count = 0; // # of subprocesses connected
+  int server_fds[MAX_CLIENTS], sem_ids[MAX_CLIENTS];
   char fifo_name[20];
+  char from_sub[BUFFER_SIZE], to_sub[BUFFER_SIZE];
 
   while(cont){
     
@@ -138,55 +138,38 @@ void mainserver(int pipe_to_main[2]){
     
       if((fd = open(fifo_name, O_RDWR | O_NONBLOCK)) > 0) {	
 	printf("[MAIN %d]: Connected to fifo [%s]\n", getpid(), fifo_name);
-	server_fds[count] = fd;
+	server_fds[sub_count] = fd;
 
 	int sub_pid = 0;
 	while(read(pipe_to_main[READ], &sub_pid, 4) <= 0);
 	
-	sem_ids[count] = create_semaphore(sub_pid);
-	printf("[MAIN %d]: Semaphore {sem_id: %d} created!\n", getpid(), sem_ids[count]);
-	count++;
+	sem_ids[sub_count] = create_semaphore(sub_pid);
+	printf("[MAIN %d]: Semaphore {sem_id: %d} created!\n", getpid(), sem_ids[sub_count]);
+	sub_count++;
       } else
 	printf("[MAIN %d]: Failed to connect to fifo [%s]\n", getpid(), fifo_name);
     }
+
     
-    int s_fd;
+    int sem_id, sub_fd;
     
-    int i;
-    
-    for(i=0; i<count; i++){
-      char from_sub[BUFFER_SIZE];
-      s_fd = server_fds[i];
-      int sem_id = sem_ids[i];
-      int n;
+    for(i=0; i<sub_count; i++){
+      sub_fd = server_fds[i];
+      sem_id = sem_ids[i];
 
       // Verify that semaphore is used (before sub writes)
-      if(is_used(sem_id) && (read(s_fd, from_sub, sizeof(from_sub)) > 0)){
+      if(is_used(sem_id) && (read(sub_fd, from_sub, sizeof(from_sub)) > 0)){
 	
-	char to_sub[BUFFER_SIZE];
-	printf("[MAIN %d]: Received bytes from sub: [%s]\n", getpid(), from_sub);
+	printf("[MAIN %d]: Received from sub [%s]\n", getpid(), from_sub);
 	
 	handle_sub_command(from_sub, &to_sub);
-	
-	// Wait for sub to open in read mode before allowing to write
-	//printf("[MAIN curr status: %d]\n", is_used(sem_id));
-	wait_semaphore(sem_id);
-	//printf("[MAIN %d]: Got semaphore resource {%d}!\n", getpid(), sem_id);
-	
-	//close(s_fd);
-	//s_fd = open(fifo_name, O_WRONLY);
-	write(s_fd, to_sub, sizeof(to_sub));
-	printf("[MAIN %d]: Wrote back to sub [%s]\n", getpid(), to_sub);
-	//close(s_fd);
 
-	// Reopen in read mode
-	//printf("now status: [%d]\n", get_sem_val(sem_id));
+	// Wait for subprocess to read before writing
+	wait_semaphore(sem_id);	
+	write(sub_fd, to_sub, sizeof(to_sub));
+	printf("[MAIN %d]: Wrote back to sub [%s]\n", getpid(), to_sub);
+
 	free_semaphore(sem_id);
-	//printf("[MAIN %d]: Freed semaphore resource {%d}!\n", getpid(), sem_id);	
-	//printf("now status 2: [%d]\n", get_sem_val(sem_id));
-	
-	
-	//s_fd = open(fifo_name, O_RDONLY | O_NONBLOCK);
       }
     }
     
@@ -194,9 +177,8 @@ void mainserver(int pipe_to_main[2]){
     // end of while
   }
 
-  int j;
-  for(j=0; j<count; j++){
-    remove_semaphore(&sem_ids[j]);
+  for(i=0; i<sub_count; i++){
+    remove_semaphore(&sem_ids[i]);
   }
 
   exit(0);
@@ -205,10 +187,11 @@ void mainserver(int pipe_to_main[2]){
 
 void subprocess(int socket, char *group_name, char* fifo_name) {
   char buffer[BUFFER_SIZE];
-  int fd, sem_id = 0, sem_id2 = 0;
+  int fd, sem_id = 0;
   
   if((fd = open(fifo_name, O_RDWR | O_NONBLOCK)) > 0) {    
     printf("[SUB %d for %s]: Connected to fifo [%s]\n", getpid(), group_name, fifo_name);
+    unlink(fifo_name);
     //close(fd);
   } else
     printf("[SUB %d for %s]: Error creating FD!\n", getpid(), group_name);
@@ -222,53 +205,28 @@ void subprocess(int socket, char *group_name, char* fifo_name) {
     int resp = handle_main_command(buffer, &write_to_client, &write_to_fifo);
     write(socket, write_to_client, sizeof(write_to_client));
 
-    // Handle writing to mainserver and receiving response
+    // Handle writing to mainserver and receive a response
     if(resp > 0) {
-      //fd = open(fifo_name, O_WRONLY);
-      //printf("[SUB %d for %s]: FIFO opened in WRONLY mode!\n", getpid(), group_name);
       
-      if(sem_id <= 0) {
+      if(sem_id <= 0)
 	sem_id = get_semaphore(getpid());
-	//printf("Accessed and received sem_id={%d}\n", sem_id);	
-	//printf("[STATUS 1 of {%d}]: [%d]\n", sem_id, is_used(sem_id));
-      }
-
-      //printf("[SUB %d for %s]: Attempting to get resource {%d}\n", getpid(), group_name, sem_id);
-      //printf("[STATUS 2]: {%d}\n", is_used(sem_id));
+      
       if(is_used(sem_id) == 0)
 	wait_semaphore(sem_id);
-      //printf("[STATUS 3]: {%d}\n", is_used(sem_id));
-      //printf("[SUB %d for %s]: Got semaphore resource {%d}!\n", getpid(), group_name, sem_id);
       
       write(fd, write_to_fifo, sizeof(write_to_fifo));
-      printf("[SUB %d for %s]: Wrote to MAIN [%s]\n", getpid(), group_name, write_to_fifo);
-      //close(fd);
-      
-      // Open in read mode
-      // Free to allow mainserver to start writing
+      printf("[SUB %d for %s]: Wrote to MAIN [%s]\n", getpid(), group_name, write_to_fifo);      
 
-      //fd = open(fifo_name, O_RDONLY | O_NONBLOCK);
-      //printf("[SUB %d for %s]: FIFO %d opened in RDONLY mode!\n", getpid(), group_name, fd);       
-      free_semaphore(sem_id);
-      //printf("[SUB %d for %s]: Freed semaphore resource {%d}\n", getpid(), group_name, sem_id);
-      
+      // Allow mainserver to write
+      free_semaphore(sem_id);      
       while(read(fd, read_from_fifo, BUFFER_SIZE) <= 0);
       printf("[SUB %d for %s]: Received [%s]\n", getpid(), group_name, read_from_fifo);
 
       // Process read_from_fifo
       handle_info_command(read_from_fifo, &write_to_client);
-      write(socket, write_to_client, sizeof(write_to_client));
-      
-      
-      //while((n = read(fd, read_from_fifo, BUFFER_SIZE)) <= 0);
-      //printf("[SUB %d for %s]: Received {%d} bytes [%s] from MAIN\n", getpid(), group_name, n, read_from_fifo);
 
-      // Reopen in write mode
-      //wait_semaphore(sem_id);
-      //printf("[SUB %d for %s]: Got semaphore resource again!\n", getpid(), group_name);
-      
-      //close(fd);
-      //fd = open(fifo_name, O_WRONLY);  
+      // Write back to client once again
+      write(socket, write_to_client, sizeof(write_to_client));
     }
   }
   
@@ -329,8 +287,8 @@ void handle_sub_command(char *s, char (*to_sub)[]){
 
 
 /* 
-Handles information sent back from mainserver to be
-sent back to client 
+   Handles information sent back from mainserver to be
+   sent back to client 
 */
 void handle_info_command(char *s, char (*to_client)[]){
   strcpy(*to_client, s);
@@ -340,7 +298,7 @@ void handle_info_command(char *s, char (*to_client)[]){
 /* 
    s: string with command arguments that need to be handled
    fifo_fd: descriptor of fifo between MAIN and SUB
-    to_client: pointer to array that will be sent back to client. Set accordingly by
+   to_client: pointer to array that will be sent back to client. Set accordingly by
    what the client inputs.
 
    RETURNS 1 IF THE SUBPROCESS NEEDS TO TALK TO THE MAINSERVER IN ADDITION TO THE CLIENT
@@ -382,24 +340,30 @@ int handle_main_command(char *s, char (*to_client)[], char (*to_fifo)[]) {
     
     /* Single phrase commmands */    
     if(strcmp(parsed[0], "@list") == 0) {
-      /* Update this !!! */      
-      //strcpy(*to_client, "#t|SERVER|chatroom1: 2 people\nchatroom2: 3 people#");
-
+      
       strcpy(*to_client, "#o|requesting#");
       strcpy(*to_fifo, "sub-wants-list");
-      return 1;      
+      return 1;
+      
     } else if(strcmp(parsed[0], "@help") == 0){
+      
       strcpy(*to_client, "#c|display-help#");
       return 0;
+      
     } else if (!strcmp(parsed[0], "@exit")) {
+      
       strcpy(*to_client, "#c|exit#");
       repeat = 0;
+      
     } else {
+      
       strcpy(*to_client, "#c|display-invalid#");      
       return 0;
+      
     }
     
   } else if (num_phrases == 2) {
+    
     char *cr_name;
     struct chatroom curr_cr;
     
@@ -416,6 +380,7 @@ int handle_main_command(char *s, char (*to_client)[], char (*to_fifo)[]) {
 	  pass = 1;
 	}	
       }
+      
       // SHOULD RETURN PORT OF RESPECTIVE CHATROOM
 
       if(!pass){
